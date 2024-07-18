8e9f196b35187703c7790c18d828a4420792aafc
preetish <pbakliwal@adobe.com>
2024-07-19 01:27:24 +0530

    secon page css

--- blocks/indices/indices.js
+++ blocks/indices/indices.js
@@ -15,10 +15,22 @@ export default function decorate(block) {
     //const block = document.querySelector('.indices.block');
 
     const articleApiUrl = "/indices-data.json";
-    const jsonDataUrl = window.origin + articleApiUrl;
+    const indicesApiUrl = "/indices-longdata.json";
     let responseData;
-    // Fetch JSON data
-    fetch(jsonDataUrl)
+
+    if (block.classList.contains("indices-type-b")) {
+        const jsonDataUrl = window.origin + indicesApiUrl;
+        fetch(jsonDataUrl)
+        .then(response => response.json())
+        .then(data => {
+            responseData = data.data;
+            // Create cards
+            createCards2(responseData.slice(0, 8));
+        })
+        .catch(error => console.error('Error fetching JSON data:', error));
+    } else {
+        const jsonDataUrl = window.origin + articleApiUrl;
+        fetch(jsonDataUrl)
         .then(response => response.json())
         .then(data => {
             responseData = data.data;
@@ -26,6 +38,18 @@ export default function decorate(block) {
             createCards(responseData.slice(0, 3));
         })
         .catch(error => console.error('Error fetching JSON data:', error));
+    }
+
+    function fetchData(jsonDataUrl) {
+        // Fetch JSON data
+        fetch(jsonDataUrl)
+            .then(response => response.json())
+            .then(data => {
+                return data.data;
+                // Create cards
+            })
+            .catch(error => console.error('Error fetching JSON data:', error));
+    }
 
 
 
@@ -63,6 +87,40 @@ export default function decorate(block) {
             content2.innerHTML = content2.innerHTML.replace("{mainValues}", item.mainValues);
             content2.innerHTML = content2.innerHTML.replace("{subValues}", item.subValues);
             // Append the cloned block to the parent element
+            block.parentNode.appendChild(clone);
+
+        });
+    }
+
+    function createCards2(data){
+        data.forEach((item, i) => {
+            const clone = block.cloneNode(true);
+
+            // Add a unique class to the cloned block
+            clone.classList.add(`indices-clone`);
+            clone.classList.remove('indices');
+            clone.classList.remove('block');
+            const h2 = clone.querySelector('h2');
+            if (h2) {
+                h2.innerHTML = item.title; // Assuming 'title' is a property of 'item'
+                h2.id = "indices-title-" + i;
+            }
+
+            const content = clone.getElementsByClassName("indices-content-0")[0];
+            content.innerHTML = content.innerHTML.replace("{option}",item.option);
+            content.innerHTML = content.innerHTML.replace("{tag}",item.tag);
+
+            const content2 = clone.getElementsByClassName("indices-content-2")[0];
+            content2.innerHTML = content2.innerHTML.replace("{option-date}", item.optionDate);
+            
+            const content3 = clone.getElementsByClassName("indices-content-3")[0];
+            content3.innerHTML = content3.innerHTML.replace("{recoprice}", item.recoprice);
+            content3.innerHTML = content3.innerHTML.replace("{targetprice}", item.targetprice);
+            // Append the cloned block to the parent element
+
+            const content4 = clone.getElementsByClassName("indices-content-4")[0];
+            content4.innerHTML = content4.innerHTML.replace("{potential-return}", item.potentialReturn);
+
             block.parentNode.appendChild(clone);
 
         });
