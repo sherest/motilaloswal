59109b90f88be8e437ce8ddaa18f309468f23570
preetish <pbakliwal@adobe.com>
2024-07-18 13:20:35 +0530

    page 1 css commit

--- scripts/global-script.js
+++ scripts/global-script.js
@@ -4,3 +4,26 @@ const childDivs = document.querySelectorAll('.columns-type-a > div');
 childDivs.forEach((div, index) => {
     div.classList.add(`type-a-${index}`);
 });
+
+
+const childDivs3 = document.querySelectorAll('.columns-type-b > div');
+
+// Select the parent div with the class 'columns-type-b'
+var parentDiv = document.querySelector('.columns-type-b');
+
+// Create an anchor element
+var anchor = document.createElement('div');
+anchor.className = 'content-type-a';
+
+// Append each child div to the anchor element
+childDivs3.forEach(function(div,index) {
+    if(index<4){
+        anchor.appendChild(div);
+    }
+    if(index==4){
+        div.classList.add("content-type-b");
+    }
+});
+
+// Append the anchor element to the parent div
+parentDiv.appendChild(anchor);
